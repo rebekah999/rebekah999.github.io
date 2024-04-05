098990ecf107d98c967d9206613f3d8c66a03007
Rebekah Fowler <merebekahfowler@gmail.com>
2024-04-04 20:14:38 -0500

    Add files via upload

---
+++ content.py
@@ -0,0 +1,128 @@
+
+# List of dictionaries for each of my skills
+skills_list = [
+    {
+        "id": "python-skills",
+        "image": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Website%20Icons/python-programming-language-icon.png",
+        "label": "Python",
+        "description": "Python 3",
+        "content": "Object oriented Programming, Data Manipulation and Analysis, Data Visualization, Machine Learning, Statistical Modeling, Data Cleaning, Feature Engineering, Model Evaluation and Validation, Time Series Analysis, Database Management, Rest API data retrieval, Web Development, Pandas, NumPy, Matplotlib, Seaborn, Dash, Scikit-Learn, Statsmodels, Plotly, SciPy, SQLAlchemy, Jupyter Notebook / JupyterLab"
+    },
+    {
+        "id": "sql-skills",
+        "image": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Website%20Icons/postgresql.248x256.png",
+        "label": "SQL",
+        "description": "PostgreSQL",
+        "content": "Database Management, Data Manipulation, Analytic Functions, EDA, Subqueries, Statistical Functions, pgAdmin 4"
+    },
+    {
+        "id": "tableau-skills",
+        "image": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/49a1e7d6966c1492dcff8936e4cb04b3eea45232/Website%20Icons/tableau-icon-svgrepo-com.svg",
+        "label": "Tableau",
+        "description": "Tableau Public",
+        "content": "Data Connection and Preparation, Visualization Techniques, Dashboard and Story Creation, Calculated Fields and Expressions, Mapping and Geographic Analysis, Parameters and Filters, Storytelling"
+    },
+    {
+        "id": "excel-skills",
+        "image": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Website%20Icons/microsoft-excel-icon.png",
+        "label": "Excel",
+        "description": "Microsoft 365",
+        "content": "Data Connections, Data Entry and Formatting, Formulas and Functions, Data Cleaning and Transformation, PivotTables, Charts and Graphs, Conditional Formatting, Data Analysis Tools, Data Visualization, Data Tables, Data Protection, VLOOKUP, Slicers, Interactive dashboard Design"
+    }
+]
+
+about_content = "Hello, I'm Rebekah Fowler, a freelance data analyst with two years of experience in freelancing. I specialize in Python, SQL, Tableau, and Excel, with expertise in working with financial data. My passion lies in unraveling the stories hidden within data, enabling organizations to make informed and impactful decisions."
+
+# List of dictionaries for portfolio projects
+projects_list = [
+    {
+        "image_src": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Project%20Previews/S&P%20500%20Database%20Pipeline.png",
+        "title": "S&P 500 Database Pipeline",
+        "description": "This notebook enables users to analyze SPY options trading data based on a specified expiration date. It calculates SPY's trading range for the selected date and retrieves all options that traded at the money (ATM) on that day and expire on the given date. Subsequently, it uploads both the underlying stock data and options data to a locally hosted PostgreSQL database.",
+        "button_text": "Link to GitHub Repository",
+        "button_href": "https://github.com/rebekah999/Data-Analysis-Portfolio/blob/main/%20S%26P%20500%20Database%20Pipeline.ipynb",
+        "technology": "Python"
+    },
+    {
+        "image_src": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Project%20Previews/Term%20Deposit%20Campaign.png",
+        "title": "Marketing Analysis: Term Deposit Campaign",
+        "description": "This storyboard is designed to compare the success of the current term deposit marketing campaign with the previous one, as well as to identify the demographic groups that responded best to the current campaign.",
+        "button_text": "Link to Tableau Public",
+        "button_href": "https://public.tableau.com/app/profile/fowler.rebekah/viz/MarketingAnalysis_17062759188240/TermDepositCampaign",
+        "technology": "Tableau"
+    }, 
+    {
+        "image_src": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Project%20Previews/Bike%20Store%20Sales%20Analysis.png",
+        "title": "Bike Store Sales Analysis",
+        "description": "A SQL file with queries providing insights into order processing, revenue analysis, customer behavior, inventory management, and employee performance.",
+        "button_text": "Link to GitHub Repository",
+        "button_href": "https://github.com/rebekah999/Data-Analysis-Portfolio/blob/main/Bike%20Store%20Sales%20Analysis.sql",
+        "technology": "SQL"
+    },
+    {
+        "image_src": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Project%20Previews/2022%20Milwaukee%20Property%20Sales.png",
+        "title": "2022 Milwaukee Property Sales",
+        "description": "An Excel dashboard analyzing residential property sales in Milwaukee.",
+        "button_text": "Link to GitHub Repository",
+        "button_href": "https://github.com/rebekah999/Data-Analysis-Portfolio/blob/main/2022%20Milwaukee%20Property%20Sales%20Dashboard.xlsx",
+        "technology": "Excel"
+    },
+    {
+        "image_src": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Project%20Previews/Employee%20Churn%20Prediction.png",
+        "title": "Salifort Motors - Employee Churn Prediction",
+        "description": "A Jupyter Notebook where I used a logistic regression to predict whether an employee will churn.",
+        "button_text": "Link to GitHub Repository",
+        "button_href": "https://github.com/rebekah999/Data-Analysis-Portfolio/blob/main/Salifort%20Motors%20-%20Employee%20Churn%20Prediction.ipynb",
+        "technology": "Python"
+    },
+{
+        "image_src": "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Project%20Previews/Adidas%20Dashboard.png",
+        "title": "Adidas Sales Dashboard",
+        "description": "A Tableau dashboard visualizing how operating margins charge based on the year, store locations, and product sales.",
+        "button_text": "Link to Tableau Public",
+        "button_href": "https://public.tableau.com/app/profile/fowler.rebekah/viz/Adidas_17001247399800/HomePage",
+        "technology": "Tableau"
+    }
+    # Add more projects as needed
+]
+
+# Cert descriptions
+google_cert = "Throughout this program, I delved deep into the realm of statistical analysis, Python programming, regression modeling, and machine learning. Through hands-on projects and real-world case studies, I honed my skills in data visualization using powerful tools like Jupyter Notebook and Tableau Software."
+datacamp_ds_cert = "The track commenced with a solid foundation in Python for data science. Through interactive exercises, I gained a strong grasp of essential libraries like Pandas, Seaborn, and Scikit-Learn. These exercises solidified my understanding of data manipulation techniques, data visualization best practices, and statistical analysis methods."
+datacamp_da_cert = "During this course, I learned how to import, clean, manipulate, and visualize data using popular Python libraries such as Pandas, NumPy, and Seaborn. Additionally, I worked with real-world datasets to enhance my data manipulation and exploratory data analysis skills."
+
+certificates_list = [
+    {
+        "name": "Google Advanced Data Analytics",
+        "date_issued": "2023",
+        "description": "Throughout this program, I delved deep into the realm of statistical analysis, Python programming, regression modeling, and machine learning. Through hands-on projects and real-world case studies, I honed my skills in data visualization using powerful tools like Jupyter Notebook and Tableau Software.",
+        "href": "https://coursera.org/share/bf149d6c12f26fac5349c37c6b3582eb"
+    },
+    {
+        "name": "Datacamp Associate Data Scientist in Python",
+        "date_issued": "2019",
+        "description": "The track commenced with a solid foundation in Python for data science. Through interactive exercises, I gained a strong grasp of essential libraries like Pandas, Seaborn, and Scikit-Learn. These exercises solidified my understanding of data manipulation techniques, data visualization best practices, and statistical analysis methods.",
+        "href": "https://www.datacamp.com/statement-of-accomplishment/track/123679caa06b239d7c6079b805ead208d4e12b8f?raw1"
+    },
+    {
+        "name": "Datacamp Data Analyst with Python",
+        "date_issued": "2018",
+        "description": "During this course, I learned how to import, clean, manipulate, and visualize data using popular Python libraries such as Pandas, NumPy, and Seaborn. Additionally, I worked with real-world datasets to enhance my data manipulation and exploratory data analysis skills.",
+        "href": "https://www.datacamp.com/statement-of-accomplishment/track/feeb81c30a63619614916de6cf27d6817e1d6fad?raw=1"
+    },
+    {
+        "name": "AOP Monarch High School - Online",
+        "date_issued": "2018",
+        "description": "I successfully completed my high school education in three years through homeschooling.",
+        "href": ""
+    }
+]
+# Icon Sources 
+github_icon = "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Website%20Icons/github.svg"
+linkedin_icon = "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Website%20Icons/linkedin.svg"
+medium_icon = "https://raw.githubusercontent.com/rebekah999/rebekah999.github.io/main/Website%20Icons/medium.svg"
+
+# Social Media href 
+github_link = "https://github.com/rebekah999"
+linkedin_link = "https://www.linkedin.com/in/rebekah-fowler999"
+medium_link = "https://medium.com/@rebekah.fowler"
